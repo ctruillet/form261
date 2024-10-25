8625a6e3c5c1eb571ea3f34ad9e4e13fc17b33d6
Clement Truillet <clement.truillet@irit.fr>
2024-10-25 17:56:20 +0200

    add getformName api

--- backend/controllers/formController.js
+++ backend/controllers/formController.js
@@ -1,36 +1,64 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // Chemin vers le fichier contenant les formulaires et les paramètres
-const formsData = path.join(__dirname, '../forms.json');
+const formsData = path.join(__dirname, "../forms.json");
 
 // Méthode pour obtenir la liste des formulaires
 exports.getForms = (req, res) => {
-
-    fs.readFile(formsData, 'utf8', (err, data) => {
-        if (err) return res.status(404).send('Form not found');
-        res.json(JSON.parse(data));
-      });
+	fs.readFile(formsData, "utf8", (err, data) => {
+		if (err) return res.status(404).send("Form not found");
+		res.json(JSON.parse(data));
+	});
 };
 
 // Méthode pour obtenir un formulaire spécifique
 exports.getFormsDetails = (req, res) => {
-    const formName = req.params.formName;
-  
-    fs.readFile(formsData, 'utf8', (err, data) => {
-      if (err) return res.status(500).send('Erreur lors de la lecture du fichier de formulaires');
-  
-      try {
-        const formData = JSON.parse(data);
-        const formDetails = formData.find((form) => form.name === formName);
-  
-        if (!formDetails) {
-          return res.status(404).send('Formulaire non trouvé');
-        }
-  
-        res.json(formDetails);
-      } catch (err) {
-        return res.status(500).send('Erreur lors de l\'analyse des données');
-      }
-    });
-  };
+	const formName = req.params.formName;
+
+	fs.readFile(formsData, "utf8", (err, data) => {
+		if (err)
+			return res
+				.status(500)
+				.send("Erreur lors de la lecture du fichier de formulaires");
+
+		try {
+			const formData = JSON.parse(data);
+			const formDetails = formData.find((form) => form.name === formName);
+
+			if (!formDetails) {
+				return res.status(404).send(`Formulaire ${formName} non trouvé`);
+			}
+
+			res.json(formDetails);
+		} catch (err) {
+			return res.status(500).send("Erreur lors de l'analyse des données");
+		}
+	});
+};
+
+// Méthode pour obtenir le nom d'un formulaire à partir de son field et param
+exports.getFormName = (req, res) => {
+  const fieldsName = req.params.fieldsName;
+  const paramName = req.params.paramName;
+  // return res.status(404).send(`Formulaire (${fieldsName} ${paramName}) non trouvé`);
+
+
+	fs.readFile(formsData, "utf8", (err, data) => {
+		if (err)
+			return res.status(500).send("Erreur lors de la lecture du fichier de formulaires");
+
+		try {
+			const formData = JSON.parse(data);
+			const formDetails = formData.find((form) => form.fields === fieldsName & form.param === paramName);
+
+			if (!formDetails) {
+				return res.status(404).send(`Formulaire (${fieldsName} ${paramName}) non trouvé`);
+			}
+
+			res.json(formDetails);
+		} catch (err) {
+			return res.status(500).send("Erreur lors de l'analyse des données");
+		}
+	});
+};
--- backend/routes/formRoutes.js
+++ backend/routes/formRoutes.js
@@ -5,7 +5,10 @@ const formController = require('../controllers/formController.js');
 // Route pour obtenir la liste des formulaires
 router.get('/', formController.getForms);
 
-// Route pour obtenir un formulaire spécifique
-router.get('/:formName', formController.getFormsDetails);
+// Route pour obtenir un formulaire spécifique par son nom
+router.get('/name=:formName', formController.getFormsDetails);
+
+// Route pour obtenir un formulaire spécifique par son fields et param
+router.get('/fields=:fieldsName&param=:paramName', formController.getFormName);
 
 module.exports = router;
